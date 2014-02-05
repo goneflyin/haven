28fd12b3fdcc0af6805a8be091f2daa10860dca8
Scott Pfister <scott.pfister@gmail.com>
2014-02-05 09:05:07 -0600

    add Content-Type json header to all actions in router - and fix json

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -2,10 +2,7 @@ defmodule ApplicationRouter do
   use Dynamo.Router
 
   prepare do
-    # Pick which parts of the request you want to fetch
-    # You can comment the line below if you don't need
-    # any of them or move them to a forwarded router
-    conn.fetch([:cookies, :params])
+    conn.put_resp_header "Content-Type", "application/json"
   end
 
   # It is common to break your Dynamo into many
@@ -18,6 +15,6 @@ defmodule ApplicationRouter do
   end
 
   get "/somejson" do
-    conn.resp(200, "{message: \"Pretend this is real JSON\"}")
+    conn.resp(200, "{\"message\": \"Pretend this is real JSON\"}")
   end
 end
