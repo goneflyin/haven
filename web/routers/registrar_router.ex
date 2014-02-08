f72023e7a868b7028cb729969ba2795f4c815d18
Scott Pfister <scott.pfister@gmail.com>
2014-02-07 18:05:36 -0600

    clean up and consolidate to use better elixir style

--- web/routers/registrar_router.ex
+++ web/routers/registrar_router.ex
@@ -2,30 +2,43 @@ defmodule RegistrarRouter do
   use Dynamo.Router
 
   prepare do
-    conn.fetch [:params, :body]
+    conn
+    |> fetch([:params, :body, :headers])
+    |> require_content_type_json
   end
 
-  get "*" do
+  get "/" do
     conn.put_private :result_object, [message: "from RegistrarRouter /register"]
   end
 
-  post "*" do
-    #param_keys = conn.params |> Binary.Dict.keys
-    body = conn.req_body
-    conn.resp 200, create_response(body)
-    # conn.put_private :result_object, submission
+  get "/headers" do
+    conn.put_private :result_object, conn.fetch(:headers).req_headers
+  end
+
+  post "/" do
+    conn.resp(200, create_response(conn.req_body))
   end
 
   def create_response(body) do
-    JSON.decode(body)
-      |> handle_decode
-      |> JSON.encode!
+    body = JSON.decode(body)
+             |> handle_decode
+             |> JSON.encode!
+  end
+
+  defp fetch(conn, to_fetch) do
+    conn.fetch to_fetch
+  end
+
+  defp require_content_type_json(conn) do
+    unless conn.req_headers["content-type"] == "application/json" do
+      halt!(conn.status(422).put_private(:result_object, [message: "content must be in JSON and content-type header must be set"]))
+    end
   end
 
   def handle_decode({:ok, decoded}) do
     decoded
   end
-  def handle_decode({:unexpected_error, input}) do
-    [message: "Unexpected Error! Could not parse: #{input}"]
+  def handle_decode({status, input}) do
+    {status, [message: "Unexpected Error [#{status}]", json: input]}
   end
 end
