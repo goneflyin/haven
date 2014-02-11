1497a117a0bd92eef2cb7019992cd42fb9b368ec
Scott Pfister <scott.pfister@gmail.com>
2014-02-11 08:51:00 -0600

    add record for a Service, and update Registry to have proper API and utilize record

--- lib/haven/registry.ex
+++ lib/haven/registry.ex
@@ -1,5 +1,17 @@
 defmodule Haven.Registry do
 
-  def get_string, do: "string from Haven.Registry"
+  defrecord Service, name: nil, uris: [], host: "127.0.0.1", port: 8888
+
+  def add_service(svc = Service[]) do
+    :gen_server.cast(:registry, { :add, svc })
+  end
+
+  def get_services(name) do
+    :gen_server.call(:registry, { :get, name })
+  end
+
+  def dump do
+    :gen_server.call(:registry, :dump)
+  end
 
 end
--- lib/haven/registry/server.ex
+++ lib/haven/registry/server.ex
@@ -1,34 +1,37 @@
 defmodule Haven.Registry.Server do
   use GenServer.Behaviour
 
+  alias Haven.Registry.Service
+
   def start_link(services) do
     # In Supervisor, the worker is specified by Haven.Registry.Server module
     # passing services as an argument.
     # Therefore, the supervisor will (by default) invoke the
     # Haven.Registry.Server.start_link(stack) function to START the worker.
+    # Prefacing the args with {:local, :registry} causes the server to be
+    # registered as :registry in the local node.
     :gen_server.start_link({ :local, :registry }, __MODULE__, services, [])
   end
 
-  def init(services) do
-    { :ok, services }
+  def init(_services) do
+    { :ok, HashDict.new }
   end
 
-  def handle_call(:pop, _from, [svc|rest]) do
+  def handle_call({:get, svc_name}, _from, services) do
     # Handles async call to :gen_server.cast(pid, :tst)
     # Ignoring the calling process (_from) but gen_server doesn't!
     # Passing in the list/set of services as state
     # Will process and reply back to calling process which is waiting for reply
-    { :reply, "You popped and here it is: #{svc}", rest }
-  end
-  def handle_call(:peek, _from, [svc|rest]) do
-    { :reply, "You peeked and here it is: #{svc}", [svc|rest] }
+    { :reply, HashDict.get(services, svc_name, []), services }
   end
   def handle_call(:dump, _from, services) do
     { :reply, services, services }
   end
 
-  def handle_cast({ :push, service }, services) do
-    # Handles async fire-and-forget call to :gen_server.cast(pid, { :tst_cast, <service> })
-    { :noreply, [service|services] }
+  def handle_cast({ :add, service = Service[name: svc_name] }, services) do
+    # Handles async fire-and-forget call to :gen_server.cast(pid, { :add, <service> })
+    # TODO: verify service instance is not already registered
+    svcs = [ service | HashDict.get(services, svc_name, []) ]
+    { :noreply, HashDict.put(services, service.name, svcs) }
   end
 end
