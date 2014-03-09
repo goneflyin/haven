7ea2f5d6039d8202adc13efd9af7930e0049b859
Scott Pfister <scott.pfister@gmail.com>
2014-03-09 17:24:22 -0500

    move proxy behavior into ProxyHandler module

---
+++ lib/haven/proxy_handler.ex
@@ -0,0 +1,41 @@
+defmodule ProxyHandler do
+
+  alias Haven.Registry
+  alias Haven.Registry.Service
+
+  def handle(conn) do
+    conn = conn.delete_resp_header "cache-control"
+    conn = conn.delete_resp_header "connection"
+    Registry.get_services_by_uri(conn.path_info_segments())
+      |> _handle(conn)
+  end
+
+  def _handle([], conn) do
+    conn.status(418)
+  end
+  def _handle([Service[host: host, port: port] | _], conn) do
+    conn = conn.fetch([:cookies, :params, :headers, :body])
+    # scheme = conn.scheme
+    url = '#{conn.scheme}://#{host}:#{port}#{conn.path}?#{conn.query_string}'
+    headers = convert_keys_to_atoms conn.req_headers
+    # cookies = conn.req_cookies
+    body = conn.req_body
+    method = conn.method |> String.downcase |> binary_to_atom
+    :ibrowse.send_req(url, headers, method, body) |> relay_response(conn)
+  end
+
+  def convert_keys_to_atoms([]), do: []
+  def convert_keys_to_atoms([{dynamo_header_key, dynamo_header_value} | rest]) do
+    [{binary_to_atom(dynamo_header_key), dynamo_header_value} | convert_keys_to_atoms(rest)]
+  end
+  def convert_keys_to_atoms(dict) do
+    convert_keys_to_atoms(Binary.Dict.to_list(dict))
+  end
+
+  def relay_response({:ok, status, headers, body}, conn) do
+    Enum.reduce headers, conn, fn({header_key, value}, conn) -> conn.put_resp_header(header_key, value) end
+    conn = status |> list_to_integer |> conn.status
+    conn = body |> list_to_bitstring |> conn.resp_body
+    conn
+  end
+end
--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -3,62 +3,34 @@ require IEx
 defmodule ApplicationRouter do
   use Dynamo.Router
 
-  alias Haven.Registry
-  alias Haven.Registry.Service
-
   @header "X-Haven-REST-Proxy"
+
+  prepare do
+    conn.put_resp_header(@header, "true")
+  end
 
   forward "/services", to: RegistrarRouter
 
   match "/*" do
-    conn = conn.put_resp_header(@header, "true")
-    conn = conn.delete_resp_header "cache-control"
-    conn = conn.delete_resp_header "connection"
-    # Apex.ap conn.resp_headers
-    Registry.get_services_by_uri(conn.path_info_segments())
-      |> handle(conn)
-    # conn.resp 200, "Would forward to: #{conn.path_info_segments()}"
-  end
-
-  def handle([], conn) do
-    conn.status(418)
-  end
-  def handle([Service[host: host, port: port] | _], conn) do
-#    Apex.ap service
-    conn = conn.fetch([:cookies, :params, :headers, :body])
-    # scheme = conn.scheme
-    url = '#{conn.scheme}://#{host}:#{port}#{conn.path}?#{conn.query_string}'
-    headers = convert_keys_to_atoms conn.req_headers
-    # cookies = conn.req_cookies
-    body = conn.req_body
-    method = conn.method |> String.downcase |> binary_to_atom
-#    IO.puts "Url: #{inspect url}"
-#    IO.puts "Scheme: #{inspect scheme}"
-#    IO.puts "Headers: "
-#    Apex.ap headers
-#    IO.puts "Method: #{inspect method}"
-#    IO.puts "Body: #{inspect body}"
-    # IEx.pry
-    :ibrowse.send_req(url, headers, method, body) |> relay_response(conn)
-    # conn.status 200, "dunno what im a doin here"
-  end
-
-  def convert_keys_to_atoms([]), do: []
-  def convert_keys_to_atoms([{dynamo_header_key, dynamo_header_value} | rest]) do
-    [{binary_to_atom(dynamo_header_key), dynamo_header_value} | convert_keys_to_atoms(rest)]
-  end
-  def convert_keys_to_atoms(dict) do
-    convert_keys_to_atoms(Binary.Dict.to_list(dict))
-  end
-
-  def relay_response({:ok, status, headers, body}, conn) do
-#    IO.puts "status returned"
-#    IO.inspect status
-#    IO.puts "headers returned"
-#    IO.inspect headers
-    Enum.reduce headers, conn, fn({header_key, value}, conn) -> conn.put_resp_header(header_key, value) end
-    conn = status |> list_to_integer |> conn.status
-    conn = body |> list_to_bitstring |> conn.resp_body
-    # conn = conn.resp_body("Body will go here")
+    ProxyHandler.handle(conn)
   end
 end
+
+
+# AppRouter -- just forwards registry calls to the registry router and handlers to the handler router
+# Registry handler -- registers the service info, which means...
+#    - creates/passes-data-on-to ServiceMonitors
+#    - indexes which service is handled by each registered URI
+#    - raises problems when conflicts appear, e.g. a service tries to register for a URI that is already handled
+# Handler router -- calls Handler.prehandle for all calls
+# Handler
+#    - prehandle will say...
+#        :ok for handleable calls,
+#        :unknown for calls where the route has no handler
+#        :response for calls where some signal implied a data/service query and the data is being provided
+#             (or will it? wouldn't it just handle these itself? so it's pretty much :ok or :unknown)
+#    - handle will take the call data, make the actual call, and return / process the response
+# ServiceMonitor -- known by name, monitors all instances, knowns all URIs that should be handled
+# InstanceMonitor -- known by host/port, only directly known by ServiceMonitor, can send messages to
+#   ServiceMonitor when responses are in certain ranges or health checks come back as insufficiently healthy
+#   Will make health check calls
