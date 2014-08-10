b46618f8946b33511b974ebec1d5cbeb7cf42880
Scott Pfister <scott.pfister@gmail.com>
2014-08-10 12:21:26 -0500

    migrate to plug and hex-based dependencies

--- lib/haven.ex
+++ lib/haven.ex
@@ -6,13 +6,7 @@ defmodule Haven do
   application and its Dynamos.
   """
   def start(_type, args) do
-    :application.start(:ssl)
-    :application.start(:ibrowse)
-    # Enum.each [:ssl, :ibrowse], &Application.Behaviour.start(&1)
     start_registry(args[:registry])
-    # TODO: Eventually need to have a single root supervisor and include
-    #   the Dynamo and Registry supervisors both in that supervisor tree.
-    Haven.Dynamo.start_link([max_restarts: 5, max_seconds: 5])
   end
 
   def start_registry(registry_args) do
--- mix.exs
+++ mix.exs
@@ -3,26 +3,25 @@ defmodule Haven.Mixfile do
 
   def project do
     [ app: :haven,
-      version: "0.0.1",
+      version: "0.1.0",
+      elixir: "~> 0.15.0",
       build_per_environment: true,
-      dynamos: [Haven.Dynamo],
-      compilers: [:elixir, :dynamo, :app],
       deps: deps ]
   end
 
   # Configuration for the OTP application
   def application do
     [ registered: [:registry],
-      applications: [:crypto, :public_key, :ssl, :cowboy, :dynamo],
+      applications: [:cowboy, :plug],
       mod: { Haven, [ registry: [:shopping_cart_svc, :identity_api, :collections_svc] ] } ]
   end
 
   defp deps do
-    [ { :cowboy,                    github: "extend/cowboy" },
-      { :dynamo,    "~> 0.1.0-dev", github: "goneflyin/dynamo", branch: "defexception-fix" },
-      { :json,                      github: "cblage/elixir-json"},
-      { :httpotion, "~> 0.2.3",     github: "myfreeweb/httpotion" },
-      { :ex_doc,                    github: "elixir-lang/ex_doc" },
-      { :apex,      "~>0.3.0",      github: "BjRo/apex"} ]
+    [ { :cowboy,    "~> 1.0.0"                              },
+      { :plug,      "~> 0.5.3"                              },
+      { :json,      "~> 0.3.0"                              },
+      { :httpotion, "~> 0.2.3",                             },
+      { :ex_doc,    "~> 0.5.1",                             },
+      { :apex,      "~>0.3.0",                              } ]
   end
 end
