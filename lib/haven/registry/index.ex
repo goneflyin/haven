268c73462a29106ebfab21c849f977407629c3fe
Scott Pfister <scott.pfister@gmail.com>
2014-08-24 08:33:20 -0500

    remove unused files

--- lib/haven/registry/index.ex
+++
@@ -1,7 +0,0 @@
-defmodule Haven.Registry.Index do
-  def get_service_for_name({name_index, _}, name) do
-    []
-  end
-
-  # def add_instance_for_uri({name_index, uri_index}, {name, host, port}, 
-end
